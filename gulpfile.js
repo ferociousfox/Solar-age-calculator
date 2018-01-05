6a5c90d290d14741a576dfa469bb4272179593cb
Shane Ryan <onyx.shane@gmail.com>
2018-01-05 11:40:54 -0800

    add minifyScripts jsBrowserify and concatInterface tasks to gulpfile.js these will minify scripts make all files ready for the browser and concat all -interface.js files respectively

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,49 @@
-
-
-
-
-
-
-
-
+//variables dependencies//
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
 var jshint = require('gulp-jshint');
-
-
-
-
+var browserSync = require('browser-sync').create();
+var buildProduction = utilities.env.production;
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap": {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 //linter to run on all files in js folder//
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+//concatenating all -interface.js and placing them in tmp folder//
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+//making all files ready for the browser//
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+//minifying scripts, Browserify must run first - then place in build folder//
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
