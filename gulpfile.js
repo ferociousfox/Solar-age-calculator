a6689c571a77807cf43426ef8bb3392a97f2acdc
Shane Ryan <onyx.shane@gmail.com>
2018-01-05 15:02:09 -0800

    add functioning server with connected front-end and back-end

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-//variables dependencies//
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
@@ -7,13 +6,13 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var babelify = require('babelify');
 var browserSync = require('browser-sync').create();
-var buildProduction = utilities.env.production;
-var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var buildProduction = utilities.env.production;
 var lib = require('bower-files')({
   "overrides":{
-    "bootstrap": {
+    "bootstrap" : {
       "main": [
         "less/bootstrap.less",
         "dist/css/bootstrap.css",
@@ -22,6 +21,7 @@ var lib = require('bower-files')({
     }
   }
 });
+
 //linter to run on all files in js folder//
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
@@ -31,46 +31,46 @@ gulp.task('jshint', function(){
 //concatenating all -interface.js and placing them in tmp folder//
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
-    .pipe(concat('allConcat.js'))
-    .pipe(gulp.dest('./tmp'));
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
 });
 //making all files ready for the browser//
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ['./tmp/allConcat.js'] })
-  .transform(babelify.configure({
+    .transform(babelify.configure({
       presets: ['es2015']
     }))
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
-//minifying scripts, Browserify must run first - then place in build folder//
-gulp.task("minifyScripts", ["jsBrowserify"], function(){
-  return gulp.src("./build/js/app.js")
+//minifies scripts, then places them into build folder//
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src('./build/js/app.js')
     .pipe(uglify())
-    .pipe(gulp.dest("./build/js"));
+    .pipe(gulp.dest('./build/js'));
 });
-//use bower to concat and minify all vendor js files//
-gulp.task('bowerJS', function() {
+//use bower to concat and minify vendor js files//
+gulp.task('bowerJS', function () {
   return gulp.src(lib.ext('js').files)
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
 });
-//use bower to concat and minify all vendor css files //
-gulp.task('bowerCSS', function() {
+//use bower to concat and minify vendor css files//
+gulp.task('bowerCSS', function () {
   return gulp.src(lib.ext('css').files)
     .pipe(concat('vendor.css'))
     .pipe(gulp.dest('./build/css'));
 });
-//run bowerCSS and bowerJS//
+//runs bowerJS and BowerCSS//
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
-//clean up the old build and tmp folders//
+//deletes build and tmp folders//
 gulp.task("clean", function(){
   return del(['build', 'tmp']);
 });
-//for build for production and cleaning the files//
-gulp.task("build", ['clean'], function(){
+//for build production and cleaning of the files//
+gulp.task('build', ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
@@ -79,21 +79,21 @@ gulp.task("build", ['clean'], function(){
   gulp.start('bower');
   gulp.start('cssBuild');
 });
-//build a local server to work on your development build//
-gulp.task('serve', function() {
+//build and local server to work on development build//
+gulp.task('serve', ['build'], function() {
   browserSync.init({
     server: {
       baseDir: "./",
       index: "index.html"
     }
   });
-  // set up gulp watch to update the server//
+  //sets up gulp watch to update the server//
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
-  gulp.watch("scss/*.scss", ['cssBuild']);
+  gulp.watch('scss/*.scss', ['cssBuild']);
 });
-//subtasks for gulp watch to reload the server//
+// has gulp watch re-build the server//
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
   browserSync.reload();
 });
@@ -102,15 +102,15 @@ gulp.task('bowerBuild', ['bower'], function(){
   browserSync.reload();
 });
 
-gulp.task('htmlBuild', function(){
+gulp.task('htmlBuild', ['bower'], function(){
   browserSync.reload();
 });
-
-gulp.task('cssBuild', function() {
-  return gulp.src('./scss/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.stream());
+//creates css in build folder//
+gulp.task('cssBuild', ['bower'], function(){
+  return gulp.src('scss/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./build/css'))
+  .pipe(browserSync.stream());
 });
---
+++ js/time-interface.js
@@ -0,0 +1,9 @@
+
+import { test } from './../js/scripts.js';
+
+$(document).ready(function(){
+  alert('document ready');
+  let backendTest = test();
+  console.log(backendTest);
+  $('h1').append(backendTest);
+});
